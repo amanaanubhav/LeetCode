7731332020023819c7ae5f8afcbdc2b2c572ce98
Aman Anubhav <138228339+amanaanubhav@users.noreply.github.com>
2025-12-10 18:12:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (20.60%)

---
+++ my-leetcode-solutions/0007-reverse-integer/solution.cpp
@@ -0,0 +1,24 @@
+#include <climits> // Required for INT_MAX and INT_MIN
+
+class Solution {
+public:
+    int reverse(int x) {
+        int revnum = 0;
+        
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            if (revnum > INT_MAX / 10 || (revnum == INT_MAX / 10 && digit > 7)) {
+                return 0;
+            }
+
+            if (revnum < INT_MIN / 10 || (revnum == INT_MIN / 10 && digit < -8)) {
+                return 0;
+            }
+
+            revnum = (revnum * 10) + digit;
+        }
+        
+        return revnum;
+    }
+};
